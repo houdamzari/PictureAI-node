f805d4ccdb9aa1d31673c40d12d395200555043f
houdamzari <houdamzari1999@gmail.com>
2023-02-08 08:39:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import GenratiorRoute from './routes/GenratiorRoute.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/dalle', GenratiorRoute);
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from PictureAI',
+  });
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
